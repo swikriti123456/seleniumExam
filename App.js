cfd016a8bbfdffb52e0153392793fdad8136b467
swikriti ranjan <swikritiranjan123@gmail.com>
2021-03-06 11:57:24 -0800

    this is my first commit

---
+++ App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import About from 'About.jsx';
+function App() {
+  return (
+    <div>
+      <h2>Welcome to home page</h2>
+      <h3>Welcome swikriti ranjan</h3>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <div>
+            <h3>this is the home page</h3>
+        </div>
+    )
+}
